d970f1ac17b87df5328f5c74d707080f0206db50
ZzzandyzzZ <andy_gean@hotmail.com>
2023-03-14 18:48:01 -0500

    Create App base component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import { Container } from '@mui/material'
-import { Searcher } from './components/searcher/Searcher'
+import { Container, Divider, Grid, Paper } from '@mui/material'
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
+
+import 'animate.css'
+import { UserProfile } from './components/User/UserProfile'
+import { UserResume } from './components/User/UserResume'
 
 function App (): JSX.Element {
   return (
-    <Container sx={{
-      backgroundColor: 'whitesmoke',
-      width: '80vw',
-      height: '80vh',
-      borderRadius: '16px',
-      marginTop: '40px',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center'
-    }}>
-      <Searcher />
+    <Container sx={{ py: 2 }}>
+      <Header />
+      <Paper elevation={5} sx={{ borderRadius: '0 0 20px 20px' }}>
+        <main>
+          <Grid container>
+            <Grid item xs={4}>
+              <UserProfile />
+            </Grid>
+            <Grid item xs={8} display='flex' alignItems='stretch'>
+              <UserResume />
+            </Grid>
+          </Grid>
+        </main>
+        <Divider />
+        <Footer />
+      </Paper>
     </Container>
   )
 }
