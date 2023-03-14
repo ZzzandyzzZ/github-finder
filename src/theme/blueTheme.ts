145c667dd883cd6493e6584a08f0d10f309c580f
ZzzandyzzZ <andy_gean@hotmail.com>
2023-03-14 18:49:48 -0500

    Create blue theme

--- src/theme/blueTheme.ts
+++ src/theme/blueTheme.ts
@@ -2,15 +2,15 @@ import { createTheme } from '@mui/material'
 
 export const blueTheme = createTheme({
   palette: {
-    mode: 'light',
-    primary: {
-      main: '#537FE7'
-    },
-    secondary: {
-      main: '#EB455F'
-    },
-    background: {
-      default: '#e9f8f9'
-    }
+    // mode: 'light',
+    // primary: {
+    //   main: '#537FE7'
+    // },
+    // secondary: {
+    //   main: '#EB455F'
+    // },
+    // background: {
+    //   default: '#e9f8f9'
+    // }
   }
 })
