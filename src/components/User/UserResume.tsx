395f08afa845348aafca614f2cf42a7abe30398f
ZzzandyzzZ <andy_gean@hotmail.com>
2023-03-17 15:08:13 -0500

    Add skeletons

--- src/components/User/UserResume.tsx
+++ src/components/User/UserResume.tsx
@@ -1,17 +1,19 @@
-import { Grid } from '@mui/material'
+import { Grid, Skeleton } from '@mui/material'
+import { useAppSelector } from '../../hooks/useStore'
 import { UserDescription } from './UserDescription'
 import { UserMedia } from './UserMedia'
 import { UserRepositories } from './UserRepositories'
 
 export const UserResume = (): JSX.Element => {
+  const { isUserLoading, isReposLoading } = useAppSelector(state => state.user)
   return (
     <Grid item container >
       <Grid item xs={12} sm={6} display='flex' flexDirection='column' justifyContent='center'>
-        <UserDescription />
-        <UserMedia />
+        {isUserLoading ? <Skeleton width='90%' height={100} sx={{ alignSelf: 'center' }}/> : <UserDescription />}
+        {isUserLoading ? <Skeleton width='90%' height={80} sx={{ alignSelf: 'center' }}/> : <UserMedia />}
       </Grid>
       <Grid item xs={12} sm={6} display='flex' flexDirection='column' justifyContent='center'>
-        <UserRepositories />
+        {isReposLoading ? <Skeleton width='90%' height='100%'sx={{ alignSelf: 'center' }}/> : <UserRepositories />}
       </Grid>
     </Grid>
   )
