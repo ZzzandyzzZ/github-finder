89d9e0ca3843c090f7e5af4eb387fe6b5f35925e
ZzzandyzzZ <andy_gean@hotmail.com>
2023-03-17 15:04:52 -0500

    Add custom theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
-import { CssBaseline } from '@mui/material'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import './index.css'
 import { store } from './store/store'
+import { blueTheme } from './theme/blueTheme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <ThemeProvider theme={blueTheme}> */}
-      <CssBaseline />
-      <App />
+      <ThemeProvider theme={blueTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 )
--- src/theme/blueTheme.ts
+++ src/theme/blueTheme.ts
@@ -2,15 +2,18 @@ import { createTheme } from '@mui/material'
 
 export const blueTheme = createTheme({
   palette: {
-    // mode: 'light',
-    // primary: {
-    //   main: '#537FE7'
-    // },
-    // secondary: {
-    //   main: '#EB455F'
-    // },
-    // background: {
-    //   default: '#e9f8f9'
-    // }
+    mode: 'light',
+    primary: {
+      main: '#192c73',
+      light: '#686DA8'
+    },
+    secondary: {
+      main: '#CDD5EA'
+    },
+    background: {
+      default: '#CDD5EA',
+      paper: '#cccbcc'
+    }
   }
+
 })
