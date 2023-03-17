805291f481f0415f9751ef101183076ff5d2267e
ZzzandyzzZ <andy_gean@hotmail.com>
2023-03-17 15:08:04 -0500

    Add component to show no repositories

--- src/components/User/UserRepositories.tsx
+++ src/components/User/UserRepositories.tsx
@@ -1,4 +1,4 @@
-import { List, ListItem, ListItemText, Stack, Typography } from '@mui/material'
+import { List, ListItem, ListItemText, Paper, Stack, Typography } from '@mui/material'
 import { useAppSelector } from '../../hooks/useStore'
 
 const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'short', day: 'numeric' }
@@ -11,16 +11,32 @@ const RepoCardData = ({ name, createdDate = 0 }: RepoCardDataProps): JSX.Element
   )
 }
 
+const ListItems = ({ userRepos }: RepoListItemsProps): JSX.Element => {
+  return (
+    <>
+      { userRepos.map(
+        repo => <RepoCardData key={repo.id} name={repo.name} createdDate={repo.createdDate}/>
+      )}
+    </>
+  )
+}
+const NoItems = (): JSX.Element => {
+  return (
+    <ListItem>
+      <ListItemText primary={'Without repositories'} />
+    </ListItem>
+  )
+}
 export const UserRepositories = (): JSX.Element => {
   const { userRepos } = useAppSelector(state => state.user)
   return (
-    <Stack>
-      <Typography variant='h5' align='center'>Last Repositories</Typography>
-      <List>
-        {userRepos.map(
-          repo => <RepoCardData key={repo.id} name={repo.name} createdDate={repo.createdDate}/>
-        )}
-      </List>
-    </Stack>
+    <Paper sx={{ m: 2 }} elevation={4}>
+      <Stack>
+        <Typography variant='h5' align='center' paddingTop={2}><strong>Last Repositories</strong></Typography>
+        <List disablePadding>
+          {userRepos.length > 0 ? <ListItems userRepos={userRepos}/> : <NoItems />}
+        </List>
+      </Stack>
+    </Paper>
   )
 }
